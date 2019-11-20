18f71a0d67b71f86eeab446f8ee2b06019718020
Joao Leite <joao_leite@hotmail.com.br>
2019-11-20 17:49:36 -0300

    create first input

--- aog/lib/main.dart
+++ aog/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:aog/widgets/logo.widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_masked_text/flutter_masked_text.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,7 +12,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.deepPurple,
       ),
       home: HomePage(),
     );
@@ -18,9 +20,30 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
+    var _gasCtrl = new MoneyMaskedTextController();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: ListView(
+        children: <Widget>[
+             Logo(),
+             TextFormField(
+                controller: _gasCtrl,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                   border: InputBorder.none,
+                ),
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 45,
+                  fontFamily: "Big Shoulder Display",
+                ),
+             ),
+        ],
+      ),
+    );
   }
 }
 
